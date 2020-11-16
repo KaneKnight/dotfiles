f5e20fd68c34561b1323a62189e5b4c8b56a3010
Kane Knight <kaneknight@Kanes-MacBook-Pro.local>
2020-11-16 15:51:13 +0000

    Alias

---
+++ zshrc.d/aliases.zsh
@@ -0,0 +1,23 @@
+# aliases.zsh
+
+# Enable color support of ls and also add handy aliases
+if [ -x "$(which dircolors)" ] ; then
+	test -r ~/.dircolors && eval "$(dircolors -b ~/.dircolors)" || eval "$(dircolors -b)"
+	alias ls='ls --color=auto'
+
+	alias grep='grep --color=auto'
+fi
+
+# ls aliases
+alias ll='ls -l'
+alias la='ls -la'
+alias l='ls -CF'
+alias lt='ls -lt'
+alias ltr='ls -ltr'
+
+# Shortcuts
+alias h=history
+alias vir='vim -R'
+
+# Git aliases
+alias gl='git --no-pager log --oneline --decorate --graph -n 20'
