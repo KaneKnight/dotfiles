d9ce03d63530e310c37848451f672dca3352fcc2
Kane <kane.knight@hotmail.com>
2022-09-06 09:42:13 +0100

    remove hoxton analytics private var

--- zshrc.d/go.zsh
+++ zshrc.d/go.zsh
@@ -1,10 +1,8 @@
 # go.zsh
 
-if [ -d "${HOME}/root/opt/go/bin" ] ; then
+if [ -d "${HOME}/root/opt/go/bin" ]; then
 	export GOROOT="${HOME}/root/opt/go"
 	export GOPATH="${HOME}/workbench/go"
-	export GOPRIVATE="github.com/hoxtonanalytics"
 
 	PATH="${GOROOT}/bin:${GOPATH}/bin${PATH:+:${PATH}}"
 fi
-
