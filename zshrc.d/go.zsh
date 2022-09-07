353446576fabecce1b766220abcfd7f0bf80a53a
Kane <kane.knight@hotmail.com>
2022-09-07 16:48:31 +0100

    add go vars for install via brew

--- zshrc.d/go.zsh
+++ zshrc.d/go.zsh
@@ -1,8 +1,19 @@
 # go.zsh
 
+# if installed manually
 if [ -d "${HOME}/root/opt/go/bin" ]; then
+	echo "installed go manually"
 	export GOROOT="${HOME}/root/opt/go"
 	export GOPATH="${HOME}/workbench/go"
+
+	PATH="${GOROOT}/bin:${GOPATH}/bin${PATH:+:${PATH}}"
+fi
+
+# if installed via brew install
+if [ -f "/usr/local/bin/go" ]; then
+	echo "installed go via brew"
+	export GOROOT="/usr/local/opt/go/libexec"
+	export GOPATH="${HOME}/go"
 
 	PATH="${GOROOT}/bin:${GOPATH}/bin${PATH:+:${PATH}}"
 fi
