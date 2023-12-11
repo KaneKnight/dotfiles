01115ee9e06743051e1bc04d183695198d206aa0
Kane Knight <tech@kaneknight.com>
2023-12-11 18:44:44 +0000

    bun

---
+++ zshrc.d/bun.zsh
@@ -0,0 +1,11 @@
+# bun.zsh
+
+if [ -d "$HOME/.bun" ] ; then
+    echo "bun"
+    # bun completions
+    [ -s "$HOME/.bun/_bun" ] && source "$HOME/.bun/_bun"
+
+    # bun
+    export BUN_INSTALL="$HOME/.bun"
+    export PATH="$BUN_INSTALL/bin:$PATH"
+fi
